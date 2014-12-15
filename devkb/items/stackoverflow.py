ee877631a93175599ef0601f92f48b8fa1e9ce21
Michael Ding <yandy.ding@gmail.com>
2014-12-15 23:43:20 +0800

    add models

--- devkb/items/stackoverflow.py
+++ devkb/items/stackoverflow.py
@@ -10,12 +10,12 @@ import scrapy
 
 class UserItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    id = scrapy.Field()
     name = scrapy.Field()
     reputation = scrapy.Field()
-    # questions = scrapy.Field()
-    # answers = scrapy.Field()
-    # tags = scrapy.Field()
+    questions = scrapy.Field()
+    answers = scrapy.Field()
+    tags = scrapy.Field()
 
 class TagItem(scrapy.Item):
     # define the fields for your item here like:
@@ -25,19 +25,19 @@ class TagItem(scrapy.Item):
 
 class QuestionItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    id = scrapy.Field()
     title = scrapy.Field()
     body = scrapy.Field()
     tags = scrapy.Field()
     vote = scrapy.Field()
-    # user = scrapy.Field()
-    # answers = scrapy.Field()
+    user = scrapy.Field()
+    answers = scrapy.Field()
 
 class AnswerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    ansid = scrapy.Field()
     body = scrapy.Field()
     vote = scrapy.Field()
     accept = scrapy.Field()
     comments = scrapy.Field()
-    # user = scrapy.Field()
+    user = scrapy.Field()
--- devkb/models/stackoverflow.py
+++ devkb/models/stackoverflow.py
@@ -8,3 +8,45 @@
 from sqlalchemy import Column, Integer, String, Table, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
+from devkb.models import DeclarativeBase
+
+class User(DeclarativeBase):
+    __tablename__ = "stackoverflow_users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    reputation = Column(Integer)
+    questions = relationship('Question', backref='user')
+    answers = relationship('Answer', backref='user')
+    tags = Column(String) #String List
+
+class Tag(DeclarativeBase):
+    __tablename__ = "stackoverflow_tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    qcount = Column(Integer)
+    descr = Column(String)
+
+class Question(DeclarativeBase):
+    __tablename__ = 'stackoverflow_questions'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    body = Column(String)
+    tags = Column(String) #String List
+    vote = Column(Integer)
+    user_id = Column(Integer, ForeignKey('stackoverflow_users.id'))
+    answers = relationship('Answer', backref='user')
+
+class Answer(DeclarativeBase):
+    __tablename__ = "stackoverflow_answers"
+
+    id = Column(Integer, primary_key=True)
+    ansid = Column(Integer)
+    body = Column(String)
+    vote = Column(Integer)
+    accept = Column(Boolean)
+    comments = Column(String) #String List
+    user_id = Column(Integer, ForeignKey('stackoverflow_users.id'))
+    question_id = Column(Integer, ForeignKey('stackoverflow_questions.id'))
