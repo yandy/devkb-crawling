a84811e5650b9c9335de2aa5295f5550fe69b3fe
Michael Ding <yandy.ding@gmail.com>
2014-12-16 16:52:10 +0800

    add pipeline
    save data to database

--- devkb/pipelines.py
+++ devkb/pipelines.py
@@ -4,9 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from devkb.models import Session
+from devkb.items.stackoverflow import UserItem, TagItem, QuestionItem, AnswerItem
+from devkb.models.stackoverflow import User, Tag, Question, Answer
 
 class DevkbPipeline(object):
 
     def process_item(self, item, spider):
+        session = Session()
+        if isinstance(item, UserItem):
+            model = User(**item)
+        elif isinstance(item, TagItem):
+            model = Tag(**item)
+        elif isinstance(item, QuestionItem):
+            model = Question(**item)
+        elif isinstance(item, AnswerItem):
+            model = Answer(**item)
+        session.merge(model)
+        session.commit()
         return item
