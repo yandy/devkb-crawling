c41c2a7da00947b57a9d65c9d7e78d53cbda330a
Michael Ding <yandy.ding@gmail.com>
2014-12-17 18:51:13 +0800

    add rules for overstackflow spider

--- devkb/spiders/stackoverflow.py
+++ devkb/spiders/stackoverflow.py
@@ -9,12 +9,40 @@ from devkb.utils import parse_int
 from scrapy.contrib.linkextractors import LinkExtractor
 
 URL_REGEX = re.compile('|'.join(URL_REGEXS['stackoverflow'].values()))
-
+ALLOW_REGEX = re.compile(r'^http://stackoverflow\.com/(questions|users|tags)')
+DENY_REGEX = re.compile('|'.join(
+                        r'^http://stackoverflow\.com/questions/ask[/?]',
+                        r'^http://stackoverflow\.com/users/login[/?]',
+                        r'^http://stackoverflow\.com/users/logout[/?]',
+                        r'^http://stackoverflow\.com/users/filter[/?]',
+                        r'^http://stackoverflow\.com/users/authenticate[/?]',
+                        r'^http://stackoverflow\.com/users/flag-weight/',
+                        r'^http://stackoverflow\.com/users/flag-summary/',
+                        r'^http://stackoverflow\.com/users/flair[/?]',
+                        r'^http://stackoverflow\.com/users/activity/',
+                        r'^http://stackoverflow\.com/users/stats/',
+                        r'^http://stackoverflow\.com/users/.*\?tab=accounts',
+                        r'^http://stackoverflow\.com/unanswered[/?]',
+                        r'^http://stackoverflow\.com/u/',
+                        r'^http://stackoverflow\.com/messages/',
+                        r'^http://stackoverflow\.com/api/',
+                        r'^http://stackoverflow\.com/review/',
+                        r'^http://stackoverflow\.com/.*/ivc/',
+                        r'^http://stackoverflow\.com/.*\?lastactivity',
+                        r'^http://stackoverflow\.com/questions/.*answertab=',
+                        r'^http://stackoverflow\.com/questions/tagged',
+                        r'^http://stackoverflow\.com/questions/.*/answer/submit',
+                        r'^http://stackoverflow\.com/suggested-edits[/?]',
+                        r'^http://stackoverflow\.com/ajax/',
+                        r'^http://stackoverflow\.com/plugins/',
+                        r'^http://stackoverflow\.com/error',
+                        r'^http://stackoverflow\.com/gps/'
+                        ))
 
 class StackoverflowSpider(scrapy.Spider):
     name = "stackoverflow"
     start_urls = ('http://stackoverflow.com',)
-    link_extractor = LinkExtractor(allow_domains=("stackoverflow.com",))
+    link_extractor = LinkExtractor(allow_domains=("stackoverflow.com",), allow=ALLOW_REGEX, deny=DENY_REGEX)
 
     def parse(self, response):
         for link in self.link_extractor.extract_links(response):
