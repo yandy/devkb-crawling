d5039660d68ca56083099dc91acd1b32023a97ce
Michael Ding <yandy.ding@gmail.com>
2014-12-25 18:42:09 +0800

    add github to pipeline

--- devkb/pipelines.py
+++ devkb/pipelines.py
@@ -5,21 +5,27 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from devkb.models import db
-from devkb.items.stackoverflow import UserItem, TagItem, QuestionItem
+from devkb.items import stackoverflow, github
 
 
 class DevkbPipeline(object):
 
     def process_item(self, item, spider):
-        if isinstance(item, UserItem):
-            db.stackoverflow_users.update(
-                {"extid": item['extid']}, {"$set": dict(item)}, upsert=True)
-        elif isinstance(item, TagItem):
-            db.stackoverflow_tags.update(
-                {"name": item["name"]}, {"$set": dict(item)}, upsert=True)
-        elif isinstance(item, QuestionItem):
-            db.stackoverflow_questions.update(
-                {"extid": item["extid"]}, {"$set": dict(item)}, upsert=True)
-        else:
-            return item
+        if spider.name is 'stackoverflow':
+            if isinstance(item, stackoverflow.UserItem):
+                db.stackoverflow_users.update(
+                    {'extid': item['extid']}, {'$set': dict(item)}, upsert=True)
+            elif isinstance(item, stackoverflow.TagItem):
+                db.stackoverflow_tags.update(
+                    {'name': item['name']}, {'$set': dict(item)}, upsert=True)
+            elif isinstance(item, stackoverflow.QuestionItem):
+                db.stackoverflow_questions.update(
+                    {'extid': item['extid']}, {'$set': dict(item)}, upsert=True)
+        elif spider.name is 'github':
+            if isinstance(item, github.UserItem):
+                db.github_users.update(
+                    {'login': item['login']}, {'$set': dict(item)}, upsert=True)
+            elif isinstance(item, github.RepoItem):
+                db.github_repos.update(
+                    {'fullname': item['fullname']}, {'$set': dict(item)}, upsert=True)
         return item
